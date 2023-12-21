9ed8669e85abcede73835a15ee0ba99f7151e441
3380issei <3380issei@gmail.com>
2023-12-21 21:29:54 +0900

    [#10] Gymの緯度経度の型をfloat64に修正

--- model/gym.go
+++ model/gym.go
@@ -1,15 +1,11 @@
 package model
 
-import (
-	"github.com/shopspring/decimal"
-)
-
 type Gym struct {
-	ID        string          `json:"id" gorm:"primary_key"`
-	Title     string          `json:"title" gorm:"not null"`
-	Address   string          `json:"address"`
-	Latitude  decimal.Decimal `json:"latitude" gorm:"not null"`
-	Longitude decimal.Decimal `json:"longitude" gorm:"not null"`
+	ID        string  `json:"id" gorm:"primary_key"`
+	Title     string  `json:"title" gorm:"not null"`
+	Address   string  `json:"address"`
+	Latitude  float64 `json:"latitude" gorm:"not null"`
+	Longitude float64 `json:"longitude" gorm:"not null"`
 
 	UserID string `json:"user_id" gorm:"not null"`
 	User   User   `gorm:"foreignkey:UserID"`
