6617bab8e744eeea6822acf33973df2b7955c8bf
3380issei <3380issei@gmail.com>
2023-12-21 20:23:10 +0900

    [#7] UserにGymのリレーションを追加

--- model/user.go
+++ model/user.go
@@ -3,4 +3,6 @@ package model
 type User struct {
 	ID       string `json:"id" gorm:"primary_key"`
 	UserName string `json:"user_name"`
+	GymID    string `json:"gym_id"`
+	Gym      Gym    `json:"gym" gorm:"foreignkey:GymID"`
 }
